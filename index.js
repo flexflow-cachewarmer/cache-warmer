8c064374b5d331719d9315db4b9b719777aa058a
algosbiz <contact@algoseabiz.com>
2025-07-29 09:50:15 +0800

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PROXIES = {
 };
 
 const USER_AGENTS = {
-  id: "Flex&Flow-CacheWarmer-ID/1.0",
+  id: "FlexandFlow-CacheWarmer-ID/1.0",
 };
 
 const CLOUDFLARE_ZONE_ID = process.env.CLOUDFLARE_ZONE_ID;
